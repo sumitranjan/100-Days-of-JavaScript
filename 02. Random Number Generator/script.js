62242cfaf878b608e89f4cd8fea834fccc628204
Sumit Ranjan <sumitranjan.ind@gmail.com>
2022-03-27 02:06:06 +0530

    Random Number Generator

---
+++ 02. Random Number Generator/script.js
@@ -0,0 +1,16 @@
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
+
+
+const generateNumber = () => {
+    //Generate number between 1 and 10
+    const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+    number.innerHTML = randomNumber;
+}
+
+btn.addEventListener("click", generateNumber);
+
+
+//When page load , random number is generated
+generateNumber();
